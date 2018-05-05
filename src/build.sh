cc55c09ea7119829a2bf059dd9006aec66065a72
Jeffrey Lim @ bionic <jeff@lim.com.au>
2018-05-05 15:08:04 +0000

    Build against latest Javelin library.

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang++ --std=c++14 *.cpp -I ../.. -I . -O3 -DJBUILDCONFIG_FINAL -o jag -lJavelinPattern -L../../Javelin/build -pthread -lz -ldl
+clang++ --std=c++14 *.cpp ../../Javelin/Stream/StringWriter.cpp ../../Javelin/Stream/CountWriter.cpp -I ../.. -I . -O3 -DJBUILDCONFIG_FINAL -o jag -lJavelinPattern -L../../Javelin/build -pthread -lz -ldl
 strip jag
